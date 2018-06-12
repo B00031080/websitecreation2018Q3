a44e526fd677bfc8dadf8f6ad1ebe3ba2f9380ec
B00031080 <33052027+B00031080@users.noreply.github.com>
2018-06-12 16:04:23 +0200

    Adding database stuff

--- index.js
+++ index.js
@@ -1,38 +1,60 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
 const bodyParser = require('body-parser');
+const mongo = require('mongodb');
 
-const app = express();
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+let MongoClient = require('mongodb').MongoClient;
+let url = "mongodb://localhost:27017";
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("game");
+  
 
-// parse application/json
-app.use(bodyParser.json())
+    const app = express();
+    app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
 
-app.get('/game', function (req, res) {
-    res.render('game', {});
+    // parse application/x-www-form-urlencoded
+    app.use(bodyParser.urlencoded({ extended: false }))
+
+    // parse application/json
+    app.use(bodyParser.json())
+
+    app.get('/game', function (req, res) {
+        res.render('game', {});
+    });
+
+    app.post('/game', function (req, res) {
+        let userNumber1 = parseInt(req.body.userNumber1);
+        let userNumber2 = parseInt(req.body.userNumber2);
+        let computerNumber = parseInt(Math.random()*100);
+        dbo.collection("plays").insertOne({player1:userNumber1,player2:userNumber2,computer:computerNumber},function(err, respo) {
+            if (err) throw err;
+            let winningUser = (Math.abs(userNumber1 - computerNumber) <
+            Math.abs(userNumber2 - computerNumber)) ? "User 1" : "User 2";
+            res.render('game', {post:true, userNumber1: userNumber1, userNumber2: userNumber2, computerNumber: computerNumber, winningUser: winningUser});        
+          });
+    });
+
+    app.get('/results', function (req, res) {
+        dbo.collection("plays").find({}).toArray(function(err, result) {
+            if (err) throw err;
+            res.render('result', {games:result});
+          });
+    });
+
+
+    app.get('/:users', function (req, res) {
+        res.render('home', {users:req.params.users.split(",")});
+    });
+    app.get('/mysecret', (req, res) => res.send('Tu ne devrais pas être là!!!'));
+
+
+
+    app.use(express.static('client'));
+
+    app.listen(3000, () => console.log('Example app listening on port 3000!'));
 });
-app.post('/game', function (req, res) {
-    let userNumber1 = parseInt(req.body.userNumber1);
-    let userNumber2 = parseInt(req.body.userNumber2);
-    let computerNumber = parseInt(Math.random()*100);
-    let winningUser = (Math.abs(userNumber1 - computerNumber) <
-        Math.abs(userNumber2 - computerNumber)) ? "User 1" : "User 2";
-
-    res.render('game', {post:true, userNumber1: userNumber1, userNumber2: userNumber2, computerNumber: computerNumber, winningUser: winningUser});
-});
 
 
-app.get('/:users', function (req, res) {
-    res.render('home', {users:req.params.users.split(",")});
-});
-app.get('/mysecret', (req, res) => res.send('Tu ne devrais pas être là!!!'));
-
-
-
-app.use(express.static('client'));
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
